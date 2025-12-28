07307b015654c459abe7b0465194802c78d84b65
ShaharMed <shaharmedina98@gmail.com>
2025-12-28 18:23:15 +0200

    Add seed data for Vitex agnus-castus
    Insert initial seed data for active compounds.

---
+++ sql/sql/seed_vitex.sql
@@ -0,0 +1,5 @@
+-- Seed data for Vitex agnus-castus
+
+INSERT INTO active_compounds (name, source, description, pubchem_id)
+VALUES
+('Agnuside', 'Vitex agnus-castus', 'Iridoid glycoside...', 'CID_3084397');
